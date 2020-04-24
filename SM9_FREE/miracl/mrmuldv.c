9565de462c6cc7310b876373a623b34c83b88140
xiaowei <xiaowei@qt.com>
2020-04-24 14:09:30 +0800

    assembly modified to C

--- SM9_FREE/miracl/mrmuldv.c
+++ SM9_FREE/miracl/mrmuldv.c
@@ -1,80 +1,54 @@
 
-/* GCC inline assembly version for Linux64 */
+/* GCC inline assembly version for Linux64*/
 
 #include "miracl.h"
-
 
 mr_small muldiv(mr_small a,mr_small b,mr_small c,mr_small m,mr_small *rp)
 {
     mr_small q;
-    __asm__ __volatile__ (
-                          "movq %1,%%rax\n"
-                          "mulq %2\n"
-                          "addq %3,%%rax\n"
-                          "adcq $0,%%rdx\n"
-                          "divq %4\n"
-                          "movq %5,%%rbx\n"
-                          "movq %%rdx,(%%rbx)\n"
-                          "movq %%rax,%0\n"
-                          : "=m"(q)
-                          : "m"(a),"m"(b),"m"(c),"m"(m),"m"(rp)
-                          : "rax","rbx","memory"
-                          );
+    mr_large ldres,p=(mr_large)a*b+c;
+    q=(mr_small)(MR_LROUND(p/m));
+    *rp=(mr_small)(p-(mr_large)q*m);
     return q;
 }
+
+mr_small imuldiv(mr_small a,mr_small b,mr_small c,mr_small m,mr_large im,mr_small *rp)
+{
+    mr_small q;
+    mr_large ldres,p=(mr_large)a*b+c;
+    q=(mr_small)MR_LROUND(p*im);
+    *rp=(mr_small)(p-(mr_large)q*m);
+    return q;
+}
+
 
 mr_small muldvm(mr_small a,mr_small c,mr_small m,mr_small *rp)
 {
     mr_small q;
-    __asm__ __volatile__ (
-                          "movq %1,%%rdx\n"
-                          "movq %2,%%rax\n"
-                          "divq %3\n"
-                          "movq %4,%%rbx\n"
-                          "movq %%rdx,(%%rbx)\n"
-                          "movq %%rax,%0\n"
-                          : "=m"(q)
-                          : "m"(a),"m"(c),"m"(m),"m"(rp)
-                          : "rax","rbx","memory"
-                          );
+    union doubleword dble;
+    dble.h[MR_BOT]=c;
+    dble.h[MR_TOP]=a;
+
+    q=(mr_small)(dble.d/m);
+    *rp=(mr_small)(dble.d-(mr_large)q*m);
     return q;
 }
 
 mr_small muldvd(mr_small a,mr_small b,mr_small c,mr_small *rp)
 {
-    mr_small q;
-    __asm__ __volatile__ (
-                          "movq %1,%%rax\n"
-                          "mulq %2\n"
-                          "addq %3,%%rax\n"
-                          "adcq $0,%%rdx\n"
-                          "movq %4,%%rbx\n"
-                          "movq %%rax,(%%rbx)\n"
-                          "movq %%rdx,%0\n"
-                          : "=m"(q)
-                          : "m"(a),"m"(b),"m"(c),"m"(rp)
-                          : "rax","rbx","memory"
-                          );
-    return q;
+    union doubleword dble;
+    dble.d=(mr_large)a*b+c;
+
+    *rp=dble.h[MR_BOT];
+    return dble.h[MR_TOP];
 }
 
 void muldvd2(mr_small a,mr_small b,mr_small *c,mr_small *rp)
 {
-    __asm__ __volatile__ (
-                          "movq %0,%%rax\n"
-                          "mulq %1\n"
-                          "movq %2,%%rbx\n"
-                          "addq (%%rbx),%%rax\n"
-                          "adcq $0,%%rdx\n"
-                          "movq %3,%%rsi\n"
-                          "addq (%%rsi),%%rax\n"
-                          "adcq $0,%%rdx\n"
-                          "movq %%rax,(%%rsi)\n"
-                          "movq %%rdx,(%%rbx)\n"
-                          :
-                          : "m"(a),"m"(b),"m"(c),"m"(rp)
-                          : "rax","rbx","rsi","memory"
-                          );
-    
+    union doubleword dble;
+    dble.d=(mr_large)a*b+*c+*rp;
+    *rp=dble.h[MR_BOT];
+    *c=dble.h[MR_TOP];
 }
+
 
